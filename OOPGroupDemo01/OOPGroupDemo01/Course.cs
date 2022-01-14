ff395780620b05d271a04221ea1d25464bd47e43
Shane Layes <slayes1@nait.ca>
2022-01-14 11:38:29 -0700

    finished OOPdemo01

--- OOPGroupDemo01/OOPGroupDemo01/Course.cs
+++ OOPGroupDemo01/OOPGroupDemo01/Course.cs
@@ -3,10 +3,134 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
+using static System.Console;
 namespace OOPGroupDemo01
 {
     internal class Course
     {
+        #region Readonly data fields
+        
+        public readonly string CourseNo;
+        public readonly string CourseName;
+        public readonly List<string> Students = new List<string>();
+       
+
+        #endregion
+
+        #region Readonly property
+        public int StudentCount
+        {
+            get { return Students.Count; }
+        }
+        #endregion
+
+        #region Constructors
+        public Course(string courseNo, string courseName)
+        {
+            
+            //validate that courseNo is not null, 
+            //or an empty string
+            //must contain exactly 8 characters
+            //where the first four characters are letters and the last 4 are numbers
+            if(string.IsNullOrEmpty(courseNo))
+            {
+                throw new ArgumentNullException("CourseNo is required. ");
+            }
+            if(courseNo.Length != 8)
+            {
+                throw new ArgumentException("CourseNo must contain exactly 8 charaters. ");
+            }
+
+
+            CourseNo = courseNo;
+
+            //validate that courseNAme is not null or an empty string,
+            //
+            if(string.IsNullOrEmpty(courseName))
+            {
+                throw new ArgumentNullException("Course name is required. ");
+            }
+
+            CourseName = courseName;
+        }
+        #endregion
+
+        #region Instance level methods
+        public void AddStudent(string name)
+        {
+            Students.Add(name);
+        }
+        public void RemoveStudent(string name)
+        {
+            Students.Remove(name);
+        }
+
+        public bool SaveToFile(string filePath)
+        {
+            bool success = false;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))    //"using" will auto close the writer but a good habit to get into is to manually close writer
+                {
+                    //Write the course number and name to a file
+                    writer.WriteLine($"{CourseNo}");
+                    writer.WriteLine($"{CourseName}");
+                    //Write the name of all students in the course
+                    foreach (var student in Students)
+                    {
+                        writer.WriteLine(student);
+                    }
+                    writer.Close(); //manually closing writer
+                    success = true;
+                }
+            }
+            catch
+            {
+                success = false;
+            }
+            
+
+            return success;
+
+        }
+        
+
+        public bool LoadFromFile(string filePath)
+        {
+            bool success = false;
+            //Read from file 
+            try
+            {
+                using(StreamReader reader = new StreamReader(filePath))
+                {
+                    //Read the CourseNo and CourseName from the file
+                    var CourseNo = reader.ReadLine();
+                    var CourseName = reader.ReadLine();
+                    //read the student names from the list
+                    while(reader.EndOfStream == false)
+                    {
+                        string? lineData = reader.ReadLine();
+                        if(lineData!=null)
+                        {
+                            Students.Add(lineData);
+                        }
+                        //Students.Add(reader.ReadLine());
+                    }
+                }
+                success = true;
+            }
+            catch
+            {
+                success=false;
+            }
+            return success;
+        }
+
+        #endregion
+
     }
+
+
 }
+
--- OOPGroupDemo01/OOPGroupDemo01/Program.cs
+++ OOPGroupDemo01/OOPGroupDemo01/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using OOPGroupDemo01;
+using static System.Console; // Allows you to use the static methods in console class directly
+
+
 //design a class named course that has:
 //CourseNo:string
 //CourseName:string
@@ -8,3 +13,45 @@
 //DropStudent(student:string)
 //StudentCount():int
 
+
+Course cpsc1517Course = new Course("CPSC1517", "Introduction to application development");
+
+//var Course cpsc1517Course = new Course("CPSC1517", "Introduction to application development"); var is letting the compiler choose a data type. 
+                                                                                                //useful if your unsure which data type was used
+
+//Course cpsc1517Course = new ("CPSC1517", "Introduction to application development");
+
+WriteLine($"CourseNo: {cpsc1517Course.CourseNo}");
+WriteLine($"CourseName: {cpsc1517Course.CourseName}");
+WriteLine("");
+//Add students to the course 
+//cpsc1517Course.AddStudent("Aaron Fong");
+//cpsc1517Course.AddStudent("David L. Mckinley");
+//cpsc1517Course.AddStudent("Hamza Said");
+//cpsc1517Course.AddStudent("James Skrlj");
+//cpsc1517Course.AddStudent("Haseeb Memon");
+
+cpsc1517Course.LoadFromFile(@"");//loads from file
+
+
+//Display all the students in the course 
+foreach (var student in cpsc1517Course.Students)
+{
+    WriteLine(student);
+}
+WriteLine("");
+//remove 2 students from the course
+WriteLine("Enter student name to remove: ");
+
+cpsc1517Course.RemoveStudent("Hamza Said");
+cpsc1517Course.RemoveStudent("Haseeb Memon");
+
+cpsc1517Course.SaveToFile("cpsc1517.csv"); //saves to file
+
+//display the number of students 
+WriteLine($"There are now {cpsc1517Course.StudentCount} students.");
+WriteLine("");
+foreach(var student in cpsc1517Course.Students)
+{
+    WriteLine(student);
+}
